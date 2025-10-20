a2be1d84eb5c1e6ac9710974be5c6e95b457e0aa
Sridatthu <134670326+Sridatthu@users.noreply.github.com>
2025-10-20 11:16:35 +0530

    zod and common repo

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,17 +1,32 @@
 import express from "express"
 import jwt from "jsonwebtoken"
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from '@repo/backend-common/config';
 import { middleware } from "./middleware";
+import {CreateUserSchema,CreateRoomSchema,SigninSchema} from '@repo/common/types'
 const app=express();
 
 app.post("/signup",(req,res)=>{
 
+    const data=CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
     res.json({
         userId:123
     })
 })
 
 app.post("/signin",(req,res)=>{
+     const data=SigninSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
     const userId=1;
   const token=jwt.sign({userId},JWT_SECRET);
 
@@ -22,6 +37,13 @@ app.post("/signin",(req,res)=>{
 })
 
 app.post("/room",middleware,(req,res)=>{
+     const data=CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
 
     res.json({
         roomId:123
